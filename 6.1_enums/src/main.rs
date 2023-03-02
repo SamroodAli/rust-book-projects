6a299248e6273b8b6bcb8c1313f54c91497abf05
samroodAli <devsamrood@gmail.com>
2023-03-02 21:50:26 +0530

    feat: add 6.1 enums with data in them

--- 6.1_enums/src/main.rs
+++ 6.1_enums/src/main.rs
@@ -4,12 +4,23 @@ enum IppAddrKind {
     V6,
 }
 
+#[derive(Debug)]
+enum IppAddrKindWithData {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
 fn main() {
     let four = IppAddrKind::V4;
     let six = IppAddrKind::V6;
 
     route(four);
     route(six);
+
+    let other_four = IppAddrKindWithData::V4(198, 0, 0, 1);
+    let other_six = IppAddrKindWithData::V6(String::from("::1"));
+
+    println!("The other kind {:?} {:?}", other_four, other_six);
 }
 
 // we can use enums like any other type
